4496a7982205c2d01688859027130ac4ec78a072
nico cucciniello <ncucciniello@gmail.com>
2017-04-24 11:25:49 -0400

    app up and running

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+// const homeRoute = require('./routes/home.js');
+// const apiRoute = require('./routes/api.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.use('/', homeRoute);
+// app.use('/api', apiRoute);
+
+app.listen(PORT, () => console.log('Server is listening on', PORT));
