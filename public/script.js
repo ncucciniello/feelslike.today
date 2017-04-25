b9e1906642ffa5511ebf09e7dbb37dce34ea889d
nico cucciniello <ncucciniello@gmail.com>
2017-04-25 15:52:18 -0400

    add icon and string update to current weather

--- public/script.js
+++ public/script.js
@@ -9,21 +9,37 @@
   //   console.log(`long = ${long}`);
   // });
 
-  function searchCity() {
+  function getTemp() {
     // fetch(`/api?myLat=${lat}&myLong=${long}`)
     fetch(`/api`)
     .then(r => r.json())
     .then((data) => {
-      console.log(data.currently.apparentTemperature);
+      console.log(`it feels like ${data.currently.apparentTemperature} degrees right now`);
+      console.log(`and it is ${data.currently.icon}`);
+      // set temp to currnet feels like temp
       document.querySelector('#temp').innerHTML = data.currently.apparentTemperature;
+      // set icon to current weather type
+      // missing fog, clear-night, partly-cloudy-night, snow, & sleet
+      if (data.currently.icon == 'cloudy') {
+        document.querySelector('#icon').src = '/assets/rainy.png';
+        document.querySelector('#string').innerHTML = 'So many clouds right now.';
+      } else if (data.currently.icon == 'partly-cloudy-day') {
+          document.querySelector('#icon').src = '/assets/partly_cloudy.png';
+          document.querySelector('#string').innerHTML = 'The sun is trying to sneak out right now.';
+      } else if (data.currently.icon == 'rain') {
+          document.querySelector('#icon').src = '/assets/rainy.png';
+          document.querySelector('#string').innerHTML = 'Bring an umbrella on your journey.';
+      } else if (data.currently.icon == 'clear-day') {
+          document.querySelector('#icon').src = '/assets/sunny.png';
+          document.querySelector('#string').innerHTML = 'Bring your favorite sunglasses.';
+      } else if (data.currently.icon == 'wind') {
+          document.querySelector('#icon').src = '/assets/windy.png';
+          document.querySelector('#string').innerHTML = 'Hang onto you cap, it’s windy right now.';
+      }
     })
     .catch(err => console.log(err));
   }
 
-  // console.log('js is linked')
-  let data = searchCity()
 
-  // console.log(document.querySelector('#temp').innerHTML)
-  // console.log(data)
-
+  getTemp()
 })();
