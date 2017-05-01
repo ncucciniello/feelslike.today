7144c6412366844fe78d484f7743c07d38cc54e3
nico cucciniello <ncucciniello@gmail.com>
2017-05-01 13:03:04 -0400

    remove backend location API call + re write the script in jQuery

--- public/script.js
+++ public/script.js
@@ -2,85 +2,69 @@
 
 (() => {
 
-  // creates variable for an img element with an id of icon.
-  let icon = document.createElement('img');
-  icon.setAttribute('id', 'icon')
-  // creates a variable for the DOM element body
-  let body = document.querySelector('body');
-  // create blank variables for latitude and longitude
+  // Set "global" variables to be used later.
   let lat = '';
   let lng = '';
-
+  let icon = $('<img>').attr('id', 'icon');
 
   function getLocation() {
-    fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD5COJtXRtxiPkp9MERs6BJNCawEYXYC0Q`,
-    {
-      method: "POST",
-    })
-    .then((r) => {
-      return r.json()
-    })
-    .then((data) => {
-      lat = data.location.lat;
-      lng = data.location.lng;
-      if ( lat == '') {
-        document.querySelector('#temp').innerHTML = `Can not find location.`;
-      } else {
-        getTemp();
+    $.ajax({
+      url:'http://ip-api.com/json',
+      method: 'GET',
+      dataType: 'JSON',
+      success: (locationData) => {
+        lat = locationData.lat;
+        lng = locationData.lon;
+        if ( lat == '') {
+          $('#temp').html(`Can not find location.`);
+        } else {
+          getTemp();
+        }
       }
-    })
-    .catch((err) => {
-      console.log(err);
     });
-
-    // // API request on backend
-    // fetch(`/location`)
-    // .then(r => r.json())
-    // .then((data) => {
-    //   console.log(data)
-    //   // lat = data.lat;
-    //   // long = data.lon;
-    //   getTemp();
-    // })
-    // .catch(err => console.log(err));
   }
 
   function getTemp() {
-    fetch(`/weather?myLat=${lat}&myLong=${lng}`)
-    .then(r => r.json())
-    .then((data) => {
 
-      // set temp to curent feels like temp and round it to whole number.
-      document.querySelector('#temp').innerHTML = `Feels like ${Math.round(data.currently.apparentTemperature)}°F today.`;
+    $.ajax({
+      url:`/weather?myLat=${lat}&myLong=${lng}`,
+      method: 'GET',
+      dataType: 'JSON',
+      success: (tempData) => {
 
-      // create and set icon to current weather type and string to match.
-      // ** missing fog, clear-night, partly-cloudy-night, snow, & sleet **
-      body.appendChild(icon);
-      // console.log(data.currently.icon)
-      if (data.currently.icon == 'cloudy') {
-        document.querySelector('#icon').src = '/assets/cloudy.png';
-        document.querySelector('#string').innerHTML = 'So many clouds right now.';
-      } else if (data.currently.icon == 'partly-cloudy-day') {
-          document.querySelector('#icon').src = '/assets/partly_cloudy.png';
-          document.querySelector('#string').innerHTML = 'The sun is trying to sneak out right now.';
-      } else if (data.currently.icon == 'partly-cloudy-night') {
-          document.querySelector('#icon').src = '/assets/cloudy.png';
-          document.querySelector('#string').innerHTML = 'The moon is hiding behind some clouds.';
-      } else if (data.currently.icon == 'rain') {
-          document.querySelector('#icon').src = '/assets/rainy.png';
-          document.querySelector('#string').innerHTML = 'Bring an umbrella on your journey.';
-      } else if (data.currently.icon == 'clear-day') {
-          document.querySelector('#icon').src = '/assets/sunny.png';
-          document.querySelector('#string').innerHTML = 'Bring your favorite sunglasses.';
-      } else if (data.currently.icon == 'clear-night') {
-          document.querySelector('#icon').src = '/assets/cloudy.png';
-          document.querySelector('#string').innerHTML = 'The moon and stars are looking good right now.';
-      }  else if (data.currently.icon == 'wind') {
-          document.querySelector('#icon').src = '/assets/windy.png';
-          document.querySelector('#string').innerHTML = 'Hang onto you cap, it’s windy right now.';
+        // set temp to curent feels like temp and round it to whole number.
+        $('#temp').html(`Feels like ${Math.round(tempData.currently.apparentTemperature)}°F today.`);
+
+        // create and set icon to current weather type and string to match.
+        // ** missing fog, clear-night, partly-cloudy-night, snow, & sleet **
+        icon.appendTo('body');
+
+        if (tempData.currently.icon == 'cloudy') {
+          icon.attr('src', '/assets/cloudy.png');
+          $('#string').html('So many clouds right now.');
+        } else if (tempData.currently.icon == 'partly-cloudy-day') {
+            icon.attr('src', '/assets/partly_cloudy.png');
+            $('#string').html('The sun is trying to sneak out right now.');
+        } else if (tempData.currently.icon == 'partly-cloudy-night') {
+            icon.attr('src', '/assets/cloudy.png');
+            $('#string').html('The moon is hiding behind some clouds.');
+        } else if (tempData.currently.icon == 'rain') {
+            icon.attr('src', '/assets/rainy.png');
+            $('#string').html('Bring an umbrella on your journey.');
+        } else if (tempData.currently.icon == 'clear-day') {
+            icon.attr('src', '/assets/sunny.png');
+            $('#string').html('Bring your favorite sunglasses.');
+        } else if (tempData.currently.icon == 'clear-night') {
+            icon.attr('src', '/assets/cloudy.png');
+            $('#string').html('The moon and stars are looking good right now.');
+        }  else if (tempData.currently.icon == 'wind') {
+            icon.attr('src', '/assets/windy.png');
+            $('#string').html('Hang onto you cap, it’s windy right now.');
+        }
+
       }
-    })
-    .catch(err => console.log(err));
+    });
+
   }
 
   // on page load the getLocation function will run
--- routes/locationAPI.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {getLocation} = require('../services/locationAPI.js');
-
-router.get('/', getLocation, (req, res) => {
-  res.json(res.location);
-});
-
-module.exports = router;
